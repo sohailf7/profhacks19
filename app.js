edf1d725be0ec10c9b18965bc65c1c1da88a2f5d
John McAvoy <mcavoyj5@students.rowan.edu>
2018-01-19 00:35:10 -0500

    Syncs with upstream profhacks18 repo

---
+++ app.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var email = require('./routes/email');
+var user = require('./routes/user');
+var mlh = require('./routes/mlh');
+
+
+var app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('view engine', 'pug');
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/**
+* Connect to API
+*/
+
+app.use('/', routes);
+app.use('/email', email);
+app.use('/user', user);
+app.use('/mlh', mlh);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
