53009860f1ba01c1c44324be6b64ec1600dfb7ad
John McAvoy <mcavoyj5@students.rowan.edu>
2018-11-13 00:49:15 -0500

    Adds registration

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const { Pool, Client } = require('pg')
 
 const app = express();
 
 const routes = require('require-all')(path.join(__dirname, '/routes'));
+
+const pool = new Pool();
+
+global.pool = pool;
 
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "pug");
@@ -17,7 +22,10 @@ app.use("/assets/img", express.static(path.join(__dirname, "assets/img")));
 app.use("/assets/js", express.static(path.join(__dirname, "assets/js")));
 
 app.get("/", routes.home);
+app.get("/register", routes.register);
 app.get("*", routes.page_not_found);
+
+app.post("/register", routes.register);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,61 @@
-module.exports = function(req, res) {
-  res.render("register");
+module.exports = async function(req, res) {
+  if(req.method === "GET") {
+    res.render("register");
+  }
+
+  if(req.method === "POST") {
+    const post = req.body;
+    const hacker = {
+      first_name: post.first_name,
+      last_name: post.last_name,
+      phone: post.phone,
+      email: post.email,
+      school: post.school,
+      country: post.country,
+      state: post.state,
+      minor: post.minor ? true : false,
+      first_timer: post.first_timer ? true: false,
+      resume: post.resume,
+      ieee_student_member: post.ieee_student_member ? true : false,
+      ieee_member_id: post.ieee_member_id ? post.ieee_member_id : null,
+      mlh_coc: post.mlh_coc ? true : false,
+      mlh_dsn: post.mlh_dsn ? true : false,
+    };
+    try {
+      await registerHacker(hacker);
+    }
+    catch(err){
+      console.log("Hacker registration failed!", err.stack);
+      return res.render("internal_error");
+    }
+    return res.render("register",{message: "Thank You for Registering"})
+  }
+}
+
+async function registerHacker(hacker) {
+  const client = await global.pool.connect();
+  console.log(`Registering Hacker ${hacker.first_name} ${hacker.last_name}`);
+  const sql = {
+    text: `INSERT INTO hacker (first_name, last_name, phone, email, school, country, state, minor, first_timer, resume, ieee_student_member, ieee_member_id, mlh_coc, mlh_dsn)
+    VALUES                     ($1,         $2,        $3,    $4,    $5,     $6,         $7,       $8,    $9,          $10,    $11,                 $12,            $13,     $14)`,
+    values: [
+      hacker.first_name,
+      hacker.last_name,
+      hacker.phone,
+      hacker.email,
+      hacker.school,
+      hacker.country,
+      hacker.state,
+      hacker.minor,
+      hacker.first_timer,
+      hacker.resume,
+      hacker.ieee_student_member,
+      hacker.ieee_member_id,
+      hacker.mlh_coc,
+      hacker.mlh_dsn,
+    ]
+  };
+
+  console.log(sql)
+  await client.query(sql);
 }
